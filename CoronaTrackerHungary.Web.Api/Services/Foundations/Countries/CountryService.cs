928a3c9e42cb63071de480050daa0e78479c6ff4
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-13 22:57:16 +0200

    FOUNDATIONS: Initialize CountryService

---
+++ CoronaTrackerHungary.Web.Api/Services/Foundations/Countries/CountryService.cs
@@ -0,0 +1,21 @@
+﻿using CoronaTrackerHungary.Web.Api.Brokers.API;
+using CoronaTrackerHungary.Web.Api.Models.Countries;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoronaTrackerHungary.Web.Api.Services.Foundations.Countries
+{
+    public class CountryService : ICountryService
+    {
+        private readonly IApiBroker apiBroker;
+        public CountryService(IApiBroker apiBroker)
+        {
+            this.apiBroker = apiBroker;
+
+        }
+        public ValueTask<List<Country>> RetrieveAllCountrieasAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CoronaTrackerHungary.Web.Api/Services/Foundations/Countries/ICountryService.cs
@@ -0,0 +1,11 @@
+﻿using CoronaTrackerHungary.Web.Api.Models.Countries;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoronaTrackerHungary.Web.Api.Services.Foundations.Countries
+{
+    public interface ICountryService
+    {
+        ValueTask<List<Country>> RetrieveAllCountrieasAsync();
+    }
+}
