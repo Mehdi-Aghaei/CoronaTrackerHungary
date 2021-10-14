ce219c3e462a7ed642ad3df372d8954f45b0402f
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-14 21:34:49 +0200

    BROKERS: Implement Logging Broker

---
+++ CoronaTrackerHungary.Web.Api/Brokers/Logging/LoggingBroker.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace CoronaTrackerHungary.Web.Api.Brokers.Logging
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+        public LoggingBroker(ILogger<LoggingBroker> logger)=>
+            this.logger = logger;
+        
+        public void LogCritical(Exception exception)=>
+            this.logger.LogCritical(exception,exception.Message);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
+        public void LogEror(Exception exception) =>
+            this.logger.LogError(exception, exception.Message);
+
+        public void LogInformation(string message) =>
+            this.logger.LogInformation(message);
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
+        
+        }
+}
