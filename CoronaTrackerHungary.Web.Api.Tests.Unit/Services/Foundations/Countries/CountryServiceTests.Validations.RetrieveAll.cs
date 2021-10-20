3843cdc24be94575d661e733318d1d120c3038f0
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-20 13:24:19 +0200

    WIP

---
+++ CoronaTrackerHungary.Web.Api.Tests.Unit/Services/Foundations/Countries/CountryServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoronaTrackerHungary.Web.Api.Models.Countries;
+using CoronaTrackerHungary.Web.Api.Models.Countries.Exceptions;
+using Xunit;
+
+namespace CoronaTrackerHungary.Web.Api.Tests.Unit.Services.Foundations.Countries
+{
+    public partial class CountryServiceTests
+    {
+        [Fact]
+        public void ShouldThrowInvalidCountryExceptionIfCountryIsInvalid()
+        {
+            Country country = new Country();
+            var expectedInvalidCountryException = 
+                new InvalidCountryException()
+            
+
+        }
+    }
+}
---
+++ CoronaTrackerHungary.Web.Api/Models/Countries/Exceptions/InvalidCountryException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace CoronaTrackerHungary.Web.Api.Models.Countries.Exceptions
+{
+    public class InvalidCountryException : Xeption
+    {
+        public InvalidCountryException()
+            : base(message:"Invalid country. please correct the errors and try again.")
+        { }     
+    }
+}
