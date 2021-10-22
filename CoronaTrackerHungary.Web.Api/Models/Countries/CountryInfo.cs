2d97daa9b66b651db13cca0b842e1210eeaf490d
Mehdi Aghaei <mahdiaghaei355@gmail.com>
2021-10-22 23:29:48 +0200

    CODE RUB: Clean Solution

--- CoronaTrackerHungary.Web.Api/Models/Countries/CountryInfo.cs
+++ CoronaTrackerHungary.Web.Api/Models/Countries/CountryInfo.cs
@@ -1,11 +1,10 @@
-﻿using System.Text.Json.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace CoronaTrackerHungary.Web.Api.Models.Countries
 {
     public class CountryInfo
     {
-        [JsonProperty(PropertyName ="_id")]
+        [JsonProperty(PropertyName = "_id")]
         public int? Id { get; set; }
 
         [JsonProperty("iso3")]
